b1bb88a9f60cae5c8e53958549fde51a9d8b631e
Ivan Stanevich <vanya.stanevich@gmail.com>
2017-05-04 15:12:48 +0300

    constants added

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,6 +1,7 @@
 const ActionTypes = {
     GET_DIALOGS: 'GET_DIALOGS',
-    SELECT_DIALOG: 'SELECT_DIALOG'
+    SELECT_DIALOG: 'SELECT_DIALOG',
+    SEND_MESSAGE: 'SEND_MESSAGE'
 };
 
 export default ActionTypes;
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 export const API = {
-    TOKEN : '',
+    TOKEN : 'cd805604c73115261d42ad9694f184893b3f39f7b9309971e4181c60542df942ba95c5708e95794b4e6e0',
     BASE_URL : 'http://localhost:5000/',
     userId : '145772800'
 };
