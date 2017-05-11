2f348d2804cc1795ae095fb41cfcac7f8a892acc
Ivan Stanevich <vanya.stanevich@gmail.com>
2017-05-11 12:34:19 +0300

    test time helper

--- src/helpers/timeHelper.test.js
+++ src/helpers/timeHelper.test.js
@@ -1,8 +1,19 @@
 import timeHelper from './timeHelper';
 
-describe('Convert unix time to string', ()=> {
+describe('Convert unix time offset to string', ()=> {
 
-    it('Hours to string', ()=> {
+    it('Minutes to string', ()=> {
+        const time = Date.now()/1000 - 660;
+        expect(timeHelper(time)).toBe('11 min');
+    });
 
-    })
+    it('Hours to string' , () => {
+        const time = Date.now()/1000 - (60 * 60 * 4);
+        expect(timeHelper(time)).toBe('4 h');
+    });
+
+    it('Days to string' , () => {
+        const time = Date.now()/1000 - (60 * 60 * 24 * 5);
+        expect(timeHelper(time)).toBe('5 d');
+    });
 });
