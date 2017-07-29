009c83d94261bd017fbd4114f0feeec16214747c
Ivan Stanevich <vanya.stanevich@gmail.com>
2017-07-29 23:01:18 +0300

    =refactored

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 export const API = {
-    TOKEN : '5b8b8fbb72045a39c5c6c6d57d193c0ee20e510ff8a6df207ddd7952e1ab415863bd05dc979a56d0b13cc',
+    TOKEN : '',
     BASE_URL : 'http://localhost:5000/',
-    userId : '145772800'
+    userId : ''
 };
